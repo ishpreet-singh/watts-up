58ab70a93f71c8e5508045e433b1c05fcbb14661
Ishpreet <ishpreetsingh2808@gmail.com>
2020-02-12 17:00:25 -0500

    Dummy Architecture Ready

--- scripts/LoadDummyData.js
+++ scripts/LoadDummyData.js
@@ -1,48 +1,81 @@
 
-console.log("Billa Paaji Changgeeee");
+var firebaseConfig = {
+    apiKey: "AIzaSyAZiVTYeuGPk_m4w_XmR2DN-8c46QnzNtQ",
+    authDomain: "wattsup-3253d.firebaseapp.com",
+    databaseURL: "https://wattsup-3253d.firebaseio.com",
+    projectId: "wattsup-3253d",
+    storageBucket: "wattsup-3253d.appspot.com",
+    messagingSenderId: "961288482160",
+    appId: "1:961288482160:web:77b8153b19d2b87d8233b5",
+    measurementId: "G-H0G03XJQN3"
+};
 
-// var firebaseConfig = {
-//     apiKey: "AIzaSyDGDkc9NSnajB0PYFERTC18VXU5Lx88JMU",
-//     authDomain: "fir-test-9797d.firebaseapp.com",
-//     databaseURL: "https://fir-test-9797d.firebaseio.com",
-//     projectId: "fir-test-9797d",
-//     storageBucket: "fir-test-9797d.appspot.com",
-//     messagingSenderId: "721598873598",
-//     appId: "1:721598873598:web:c5bd7f679e30fd54d10aaf",
-//     measurementId: "G-XQMYR64MJB"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-// var userId = firebase.auth().currentUser;
+function writeUserData() {
+    console.log("I m called");
 
-// function writeUserData() {
-//     console.log("I m called");
-//     firebase.database().ref('/username/').set({
-//         firstname: "ish",
-//         middlename: "preet"
-//     });
-// }
+    DummyData = ""
 
-// // writeUserData();
+    firebase.database().ref('/username/').set({
+        firstname: "ish",
+        middlename: "preet"
+    });
+}
 
-// function readData() {
-//     console.log("I m called");
-//     let ref = firebase.database().ref("/group chat/");
-//     ref.on("value", (snapshot) => {
-//         console.log("Snalshot value: ", snapshot.val());
-//         var keys = [];
-//         for(var k in snapshot.val()) keys.push(k);
-//         console.log("Keys: ", keys);
-//     });
-//     // ref.on("msg_id_01", function (snapshot) {
-//     //     console.log(snapshot.val());
-//     // }, function (errorObject) {
-//     //     // console.log("The read failed: " + errorObject.code);
-//     // });
-//     console.log("ref: ", ref);
-//     // debugger;
-// }
+function deleteDataFromNode(path) {
+    nodePath = firebase.database().ref(path);
+    if(nodePath) {
+        nodePath.remove();
+    } else {
+        console.log("Path Doen't Exist !!");
+    }
+}
+
+function writeDataToNode(path, data) {
+    nodePath = firebase.database().ref(path);
+    if(nodePath) {
+        nodePath.set(data);
+    } else {
+        console.log("Path Doen't Exist !!");
+    }
+}
+
+function writeDummyData() {
+
+    console.log("Writing Dummy Data *_*\n");
+
+    // Deleting 
+    // firebase.database().ref('/').remove();
+
+    var dummyJSON = "dummy/dummy.json"
+    $.getJSON(dummyJSON, function (data) {
+        writeDataToNode(data);
+    });
+}
+
+// writeDummyData()
+// deleteDataFromNode("/group_chat/users/")
+
+// writeUserData();
+
+function readData() {
+    console.log("I m called");
+    let ref = firebase.database().ref("/group chat/");
+    ref.on("value", (snapshot) => {
+        console.log("Snalshot value: ", snapshot.val());
+        var keys = [];
+        for (var k in snapshot.val()) keys.push(k);
+        console.log("Keys: ", keys);
+    });
+    // ref.on("msg_id_01", function (snapshot) {
+    //     console.log(snapshot.val());
+    // }, function (errorObject) {
+    //     // console.log("The read failed: " + errorObject.code);
+    // });
+    console.log("ref: ", ref);
+}
 
 // readData();
