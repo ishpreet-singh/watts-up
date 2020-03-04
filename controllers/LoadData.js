4ba206d4698ef3600ed0b4d8271a7cf3534249fa
Ishpreet <ishpreetsingh2808@gmail.com>
2020-03-04 15:22:28 -0500

    Task 1 from checklist completed ~ Fetch Msgs Completed

--- controllers/LoadData.js
+++ controllers/LoadData.js
@@ -3,9 +3,18 @@ function loadData() {
     
     let path = "dummy/dummy.json";
     let rootNode = "/";
+    let user = "user_04"
     $.getJSON(path, function (data) {
+        // console.log("Path: ", path)
         writeDataToNode(rootNode, data);
+        // readDataFromNode(rootNode);
+        // readUserMsgs(user);
     });
+
+    $.getJSON(path, function (data) {
+        readUserMsgs(user);
+    });
+    
     
 }
 
--- controllers/NodeController.js
+++ controllers/NodeController.js
@@ -1,17 +1,21 @@
 // Node Controller for all functions of a Node
+var msg_count = 0
 
-function readDataFromNode(path) {
+function readDataFromNode(path, callback = () => { }, cbObj = {}) {
     nodePath = firebase.database().ref(path);
     if (nodePath) {
         nodePath.on("value", (snapshot) => {
-            // console.log("Snalshot value: ", snapshot.val());
-            // var keys = [];
-            // for (var k in snapshot.val()) keys.push(k);
-            // console.log("Keys: ", keys);
-            return snapshot.val();
+            snapshotObj = JSON.parse(JSON.stringify(snapshot.val()))
+            for (key in cbObj) {
+                if(cbObj[key]) {
+                    snapshotObj[key] = cbObj[key]
+                }
+            }
+            callback(snapshotObj);
+            // return snapshot.val();
         });
     } else {
-        console.log("Path Doen't Exist !!");
+        console.log("Path Doesn't Exist !!");
     }
 }
 
@@ -29,8 +33,55 @@ function writeDataToNode(path, data) {
 function deleteDataFromNode(path) {
     nodePath = firebase.database().ref(path);
     if (nodePath) {
-        nodePath.remove();
+        nodePath.remove()
     } else {
-        console.log("Path Doen't Exist !!");
+        console.log("Path Doen't Exist !!")
     }
+}
+
+function readAllMsgsBySender(msgObj) {
+    sender = msgObj["senderName"]
+    for (key in msgObj) {
+        if (key.includes("msg_id_")) {
+            msgId = key
+            message = msgObj[key]
+            msgSender = message["sender"]
+            if (message[sender] == "true") {
+                console.log(msgId, ":", msgSender, ":", message.msg)
+            } else {
+                console.log(msgId, ":", msgSender, ":", "Message Deleted")
+                // console.log("Msg Deleted")
+            }
+        }
+    }
+}
+
+function getAllUsers() {
+    
+}
+
+function readUserMsgs(sender) {
+    senderPath = "/group_chat/"
+    callbackObj = {
+        "senderName": sender
+    }
+    console.log("Sender Name: ", sender)
+    readDataFromNode(senderPath, readAllMsgsBySender, callbackObj)
+}
+
+function addNewMsg(msg, sender) {
+    senderPath = "/group_chat/"
+    let msg_Id = "msg_id_022"
+    // users = getAllUsers()
+    
+    // data = {
+    //     msg_Id: {
+    //         "msg": msg,
+    //         "sender": sender,
+    //         "user_01": "true",
+    //         "user_02": "true",
+    //         "user_03": "true",
+    //         "user_04": "true"
+    //     }
+    // }
 }
